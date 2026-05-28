cc6b8b12f3ea2ff03ac933dacf60450f1622767a
MozartEstLaDiBufala <169143255+MozartEstLaDiBufala@users.noreply.github.com>
2026-05-28 10:26:18 +0200

    get image in entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,6 +1,8 @@
 package entity;
 
 import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
 /**
  * Entité de base du jeu
@@ -10,4 +12,18 @@ public abstract class Entity {
 	public int m_x, m_y;				//position sur la map
 	public int m_speed;					//Déplacement de l'entité
 	public BufferedImage m_idleImage;	//Une image de l'entité
+
+    /**
+     * R�cup�ration de l'image du personnage
+     */
+    public void getEntityImage(String name) {
+        //gestion des expections
+        try {
+            m_idleImage = ImageIO.read(getClass().getResource(name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
