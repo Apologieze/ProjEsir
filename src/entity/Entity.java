d91242423f607c71c324e218e9738144c5b78b2b
Paul-Adrien Troeira <47333595+Apologieze@users.noreply.github.com>
2026-05-28 12:17:52 +0200

    Change player entity structure

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,30 +1,10 @@
 package entity;
 
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.IOException;
-
 /**
  * Entité de base du jeu
  *
  */
-public abstract class Entity {
-	public int m_x, m_y;				//position sur la map
-	public int m_speed;					//Déplacement de l'entité
-	public BufferedImage m_idleImage;	//Une image de l'entité
-	public int m_life;					// Point de vie de l'entité
-
-    /**
-     * R�cup�ration de l'image du personnage
-     */
-    public void getEntityImage(String name) {
-        //gestion des expections
-        try {
-            m_idleImage = ImageIO.read(getClass().getResource(name));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
+public abstract class Entity extends Position {
+	public int speed;					//Déplacement de l'entité
+	public int life;					// Point de vie de l'entité
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,12 +2,10 @@ package entity;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
+import manager.ImageAssetManager;
 
 /**
  * Dfintition du comportement d'un joueur
@@ -15,8 +13,9 @@ import main.KeyHandler;
  */
 public class Player extends Entity{
 
-	GamePanel m_gp;
-	KeyHandler m_keyH;
+	GamePanel gp;
+	KeyHandler keyH;
+	private BufferedImage idleImage;
 
 	/**
 	 * Constructeur de Player
@@ -24,20 +23,20 @@ public class Player extends Entity{
 	 * @param a_keyH KeyHandler, gestionnaire des touches
 	 */
 	public Player(GamePanel a_gp, KeyHandler a_keyH) {
-		this.m_gp = a_gp;
-		this.m_keyH = a_keyH;
+		this.gp = a_gp;
+		this.keyH = a_keyH;
 		this.setDefaultValues();
-		this.getEntityImage("/player/superhero.png");
+		this.idleImage = ImageAssetManager.loadImage("/player/superhero.png");
 	}
 
 	/**
 	 * Initialisation des donnes membres avec des valeurs par dfaut
 	 */
 	protected void setDefaultValues() {
-		m_x = 100;
-		m_y = 100;
-		m_speed = 4;
-		m_life = 5;
+		x = 100;
+		y = 100;
+		speed = 4;
+		life = 5;
 	}
 
 	/**
@@ -47,21 +46,21 @@ public class Player extends Entity{
 		int xAxis = 0;
 		int yAxis = 0;
 
-		if (m_keyH.upPressed) {
+		if (keyH.upPressed) {
 			yAxis -= 1;
 		}
-		if (m_keyH.downPressed) {
+		if (keyH.downPressed) {
 			yAxis += 1;
 		}
-		if (m_keyH.leftPressed) {
+		if (keyH.leftPressed) {
 			xAxis -= 1;
 		}
-		if (m_keyH.rightPressed) {
+		if (keyH.rightPressed) {
 			xAxis += 1;
 		}
 
-		m_x += xAxis * m_speed;
-		m_y += yAxis * m_speed;
+		x += xAxis * speed;
+		y += yAxis * speed;
 	}
 
 	/**
@@ -69,7 +68,7 @@ public class Player extends Entity{
 	 * @param a_g2 Graphics2D
 	 */
 	public void draw(Graphics2D a_g2) {
-		BufferedImage l_image = m_idleImage;
-		a_g2.drawImage(l_image, m_x, m_y, m_gp.TILE_SIZE, m_gp.TILE_SIZE, null);
+		BufferedImage l_image = idleImage;
+		a_g2.drawImage(l_image, x, y, gp.TILE_SIZE, gp.TILE_SIZE, null);
 	}
 }
