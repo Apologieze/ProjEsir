dbee3104a75aabe842d25818b06c74b0215afa2f
brezic14 <brvnrz2@gmail.com>
2026-05-28 15:11:36 +0200

    déclaration méthode de Enemy.java

--- src/entity/Enemy.java
+++ src/entity/Enemy.java
@@ -1,5 +1,25 @@
 package entity;
 
+import java.awt.*;
+import java.awt.Graphics2D;
+/**
+ * Represents all enemy of the game
+ */
 public abstract class Enemy extends Entity{
+    private Player player;
+
+    public Enemy(Player player){
+        this.player = player;
+        x = 200;
+        y = 200;
+    }
+
+    public abstract void shoot();
+
+    public abstract void draw(Graphics2D a_g2);
+
+    public abstract void update();
+
+
 
 }
